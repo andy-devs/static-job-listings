44ba3a73a4da32cc90ca2ce60e01062a3a23adab
andy-devs <markushin.andrew@gmail.com>
2021-11-29 12:29:28 +0600

    append logic

--- app.js
+++ app.js
@@ -3,56 +3,77 @@ const contentBlock = document.querySelector('.content-block');
 fetch('./data.json')
 	.then((res) => res.json())
 	.then((data) => {
-		let item = document.createElement('div');
-		contentBlock.appendChild(data);
+		for (let i of data) {
+			let item = document.createElement('div');
+			item.classList.add('item');
+
+			let img = document.createElement('img');
+			img.classList.add('item-image');
+			img.src = `${i.logo}`;
+
+			let itemInfo = document.createElement('div');
+			itemInfo.classList.add('item-info');
+
+			let itemInfoHead = document.createElement('div');
+			itemInfoHead.classList.add('item-info__head');
+
+			let itemInfoCompany = document.createElement('span');
+			itemInfoCompany.classList.add('item-info__company');
+			itemInfoHead.textContent = `${i.company}`;
+
+			if (i.new) {
+				let itemInfoChartNew = document.createElement('div');
+				itemInfoChartNew.classList.add(
+					'content-block__item-info__chart'
+				);
+				itemInfoChartNew.classList.add('chart-new');
+				itemInfoHead.appendChild(itemInfoChartNew);
+			}
+
+			if (i.featured) {
+				let itemInfoChartFeatured = document.createElement('div');
+				itemInfoChartFeatured.classList.add(
+					'content-block__item-info__chart'
+				);
+				itemInfoChartFeatured.classList.add('chart-featured');
+				itemInfoHead.appendChild(itemInfoChartFeatured);
+			}
+			let itemProf = document.createElement('p');
+			itemProf.classList.add('info-prof');
+			itemProf.textContent = `${i.position}`;
+
+			let itemInfoAdd = document.createElement('p');
+			itemInfoAdd.classList.add('item-info__add');
+
+			itemInfoAdd.innerHTML = `<span class="content-block__item-info__add-item">${i.postedAt}</span>
+            <i class="fas fa-circle"></i>
+            <span class="content-block__item-info__add-item">
+               ${i.contract}
+            </span>
+            <i class="fas fa-circle"></i>
+            <span class="content-block__item-info__add-item">${i.location}</span>`;
+			let itemFilter = document.createElement('div');
+			itemFilter.classList.add('item-filter');
+			i['languages'].map((value) =>
+				itemFilter.appendChild(
+					`<span class="content-block__item-filter__item">${value}</span>`
+				)
+			);
+			i['tools'].map((value) =>
+				itemFilter.appendChild(
+					`<span class="content-block__item-filter__item">${value}</span>`
+				)
+			);
+
+			item.appendChild(img);
+			item.appendChild(itemInfo);
+			item.appendChild(itemFilter);
+			itemInfo.appendChild(itemInfoHead);
+			itemInfo.appendChild(itemProf);
+			itemInfo.appendChild(itemInfoAdd);
+			itemInfoHead.appendChild(itemInfoCompany);
+
+			contentBlock.appendChild(item);
+		}
 	})
 	.catch((err) => console.error(err));
-
-<div class="content-block__item">
-	<div class="content-block__item-main">
-		<img
-			src="./images/photosnap.svg"
-			alt=""
-			class="content-block__item-image"
-		/>
-		<div class="content-block__item-info">
-			<div class="content-block__item-info__head">
-				<span class="content-block__item-info__company">Photosnap</span>
-				<div
-					class="
-										content-block__item-info__chart
-										chart-new
-									"
-				>
-					NEW!
-				</div>
-				<div
-					class="
-										content-block__item-info__chart
-										chart-feat
-									"
-				>
-					FEATURED
-				</div>
-			</div>
-
-			<p class="content-block__item-prof">Senior Frontend Developer</p>
-			<p class="content-block__item-info__add">
-				<span class="content-block__item-info__add-item">1d ago</span>
-				<i class="fas fa-circle"></i>
-				<span class="content-block__item-info__add-item">
-					Full Time
-				</span>
-				<i class="fas fa-circle"></i>
-				<span class="content-block__item-info__add-item">USA only</span>
-			</p>
-		</div>
-	</div>
-	<div class="content-block__item-filter">
-		<span class="content-block__item-filter__item">Frontend</span>
-		<span class="content-block__item-filter__item">Senior</span>
-		<span class="content-block__item-filter__item">HTML</span>
-		<span class="content-block__item-filter__item">CSS</span>
-		<span class="content-block__item-filter__item">JavaScript</span>
-	</div>
-</div>;
