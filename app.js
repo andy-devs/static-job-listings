009b99809098b364909103d6d8e265493be9185c
andy-devs <markushin.andrew@gmail.com>
2021-11-29 20:00:25 +0600

    basic filtering

--- app.js
+++ app.js
@@ -5,86 +5,102 @@ const content = document.querySelector('.content');
 let dataItem;
 let filterArr = [];
 
+const isEqual = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+
 function renderItems(data, arr) {
-	for (let i of data) {
-		let item = document.createElement('div');
-		item.classList.add('item');
-
-		let img = document.createElement('img');
-		img.classList.add('item-image');
-		img.src = `${i.logo}`;
-
-		let itemMain = document.createElement('div');
-		itemMain.classList.add('item-main');
-
-		let itemInfo = document.createElement('div');
-		itemInfo.classList.add('item-info');
-
-		let itemInfoHead = document.createElement('div');
-		itemInfoHead.classList.add('item-info__head');
-
-		let itemInfoCompany = document.createElement('span');
-		itemInfoCompany.classList.add('item-info__company');
-		itemInfoCompany.textContent = `${i.company}`;
-		itemInfoHead.appendChild(itemInfoCompany);
-
-		if (i.new == true) {
-			let itemInfoChartNew = document.createElement('div');
-			itemInfoChartNew.classList.add('item-info__chart');
-			itemInfoChartNew.classList.add('chart-new');
-			itemInfoChartNew.textContent = 'NEW!';
-			itemInfoHead.appendChild(itemInfoChartNew);
+	contentBlock.innerHTML = '';
+	if (arr) {
+		let filteredData;
+		for (let i of arr) {
+			filteredData = data.filter(
+				(value) =>
+					value['languages'].includes(i) || value['tools'].includes(i)
+			);
 		}
+		console.log(filteredData);
+		contentBlock.innerHTML = '';
+		renderItems(filteredData);
+	} else {
+		for (let i of data) {
+			let item = document.createElement('div');
+			item.classList.add('item');
 
-		if (i.featured == true) {
-			let itemInfoChartFeatured = document.createElement('div');
-			itemInfoChartFeatured.classList.add('item-info__chart');
-			itemInfoChartFeatured.classList.add('chart-feat');
-			itemInfoChartFeatured.textContent = 'FEATURED';
-			itemInfoHead.appendChild(itemInfoChartFeatured);
-			item.classList.add('featured');
+			let img = document.createElement('img');
+			img.classList.add('item-image');
+			img.src = `${i.logo}`;
+
+			let itemMain = document.createElement('div');
+			itemMain.classList.add('item-main');
+
+			let itemInfo = document.createElement('div');
+			itemInfo.classList.add('item-info');
+
+			let itemInfoHead = document.createElement('div');
+			itemInfoHead.classList.add('item-info__head');
+
+			let itemInfoCompany = document.createElement('span');
+			itemInfoCompany.classList.add('item-info__company');
+			itemInfoCompany.textContent = `${i.company}`;
+			itemInfoHead.appendChild(itemInfoCompany);
+
+			if (i.new == true) {
+				let itemInfoChartNew = document.createElement('div');
+				itemInfoChartNew.classList.add('item-info__chart');
+				itemInfoChartNew.classList.add('chart-new');
+				itemInfoChartNew.textContent = 'NEW!';
+				itemInfoHead.appendChild(itemInfoChartNew);
+			}
+
+			if (i.featured == true) {
+				let itemInfoChartFeatured = document.createElement('div');
+				itemInfoChartFeatured.classList.add('item-info__chart');
+				itemInfoChartFeatured.classList.add('chart-feat');
+				itemInfoChartFeatured.textContent = 'FEATURED';
+				itemInfoHead.appendChild(itemInfoChartFeatured);
+				item.classList.add('featured');
+			}
+			let itemProf = document.createElement('p');
+			itemProf.classList.add('item-prof');
+			itemProf.textContent = `${i.position}`;
+
+			let itemInfoAdd = document.createElement('p');
+			itemInfoAdd.classList.add('item-info__add');
+
+			itemInfoAdd.innerHTML = `<span class="content-block__item-info__add-item">${i.postedAt}</span>
+			<i class="fas fa-circle"></i>
+			<span class="content-block__item-info__add-item">
+			   ${i.contract}
+			</span>
+			<i class="fas fa-circle"></i>
+			<span class="content-block__item-info__add-item">${i.location}</span>`;
+			let itemFilter = document.createElement('div');
+			itemFilter.classList.add('item-filter');
+			i['languages'].map((value) => {
+				let itemFilterElem = document.createElement('span');
+				itemFilterElem.classList.add('item-filter__item');
+				itemFilterElem.textContent = `${value}`;
+				itemFilterElem.onclick = filterItems;
+				itemFilter.appendChild(itemFilterElem);
+			});
+			i['tools'].map((value) => {
+				let itemFilterElem = document.createElement('span');
+				itemFilterElem.classList.add('item-filter__item');
+				itemFilterElem.textContent = `${value}`;
+				itemFilterElem.onclick = filterItems;
+
+				itemFilter.appendChild(itemFilterElem);
+			});
+
+			item.appendChild(itemMain);
+			itemMain.appendChild(img);
+			itemMain.appendChild(itemInfo);
+			item.appendChild(itemFilter);
+			itemInfo.appendChild(itemInfoHead);
+			itemInfo.appendChild(itemProf);
+			itemInfo.appendChild(itemInfoAdd);
+
+			contentBlock.appendChild(item);
 		}
-		let itemProf = document.createElement('p');
-		itemProf.classList.add('item-prof');
-		itemProf.textContent = `${i.position}`;
-
-		let itemInfoAdd = document.createElement('p');
-		itemInfoAdd.classList.add('item-info__add');
-
-		itemInfoAdd.innerHTML = `<span class="content-block__item-info__add-item">${i.postedAt}</span>
-		<i class="fas fa-circle"></i>
-		<span class="content-block__item-info__add-item">
-		   ${i.contract}
-		</span>
-		<i class="fas fa-circle"></i>
-		<span class="content-block__item-info__add-item">${i.location}</span>`;
-		let itemFilter = document.createElement('div');
-		itemFilter.classList.add('item-filter');
-		i['languages'].map((value) => {
-			let itemFilterElem = document.createElement('span');
-			itemFilterElem.classList.add('item-filter__item');
-			itemFilterElem.textContent = `${value}`;
-			itemFilterElem.onclick = filterItems;
-			itemFilter.appendChild(itemFilterElem);
-		});
-		i['tools'].map((value) => {
-			let itemFilterElem = document.createElement('span');
-			itemFilterElem.classList.add('item-filter__item');
-			itemFilterElem.textContent = `${value}`;
-			itemFilterElem.onclick = filterItems;
-
-			itemFilter.appendChild(itemFilterElem);
-		});
-
-		item.appendChild(itemMain);
-		itemMain.appendChild(img);
-		itemMain.appendChild(itemInfo);
-		item.appendChild(itemFilter);
-		itemInfo.appendChild(itemInfoHead);
-		itemInfo.appendChild(itemProf);
-		itemInfo.appendChild(itemInfoAdd);
-
-		contentBlock.appendChild(item);
 	}
 }
 
@@ -96,12 +112,46 @@ fetch('./data.json')
 	})
 	.catch((err) => console.error(err));
 
+function deleteFilterItem(e) {
+	let name = e.target.parentNode.previousSibling.textContent;
+	if (filterArr.includes(name)) {
+		filterArr = filterArr.filter((value) => value != name);
+	}
+	e.target.parentNode.parentNode.remove();
+	if (filterArr == 0) {
+		content.style.paddingTop = '2rem';
+		filterBlock.style.display = 'none';
+		renderItems(dataItem);
+	} else {
+		renderItems(dataItem, filterArr);
+	}
+}
+
+function createFilterBlock(name) {
+	let filterItem = document.createElement('div');
+	filterItem.classList.add('filter-block__item');
+	let filterItemText = document.createElement('span');
+	filterItemText.classList.add('filter-block__item-text');
+	filterItemText.textContent = `${name}`;
+	let filterItemImageBlock = document.createElement('div');
+	filterItemImageBlock.classList.add('filter-block__item-image');
+	let filterItemImage = document.createElement('img');
+	filterItemImage.classList.add('filter-block__item-image__item');
+	filterItemImage.src = './images/icon-remove.svg';
+	filterItemImageBlock.appendChild(filterItemImage);
+	filterItemImageBlock.onclick = deleteFilterItem;
+	filterItem.appendChild(filterItemText);
+	filterItem.appendChild(filterItemImageBlock);
+	return filterItem;
+}
+
 function filterItems(e) {
-	console.log('lol');
-	contentBlock.innerHTML = '';
-	filterArr.push(e.target.textContent);
-	content.style.paddingTop = '0rem';
-	renderItems(dataItem, filterArr);
-	filterBlock.style.display = 'flex';
-	console.log(filterArr);
+	if (!filterArr.includes(e.target.textContent)) {
+		contentBlock.innerHTML = '';
+		filterArr.push(e.target.textContent);
+		filterBlock.appendChild(createFilterBlock(e.target.textContent));
+		content.style.paddingTop = '0rem';
+		filterBlock.style.display = 'flex';
+		renderItems(dataItem, filterArr);
+	}
 }
